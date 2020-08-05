74671c4166bf804e69a969f22c8385ad84f6c037
“m“marc <3082882959@qq.com>
2020-08-06 01:39:21 +0800

    fix

---
+++ Assets/Scripts/ObjectSingle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSingle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Collider selfCollider;
+    void Awake(){
+        selfCollider = this.gameObject.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
