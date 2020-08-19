c8eac53be57a69e3627c184f0cae02e8937f4ef7
songtiefei <song_tiefei@126.com>
2020-08-19 17:32:37 +0800

    fix

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public GameObject BeginRoot;
+
+    public GameObject BattleRoot;
+    public Text numLbale;
+    public Text timeLabel;
+
+    public GameObject EndRoot;
+
+    public int currentState = 0; //0:idle,1:begin,2:end
+    public int currentNum = 0;
+    public int currentTime = 0;
+
+    public void RefreshGame () {
+        currentState = 0;
+        BeginRoot.SetActive (true);
+        BattleRoot.SetActive (false);
+        EndRoot.SetActive (false);
+
+    }
+
+    public void BeginGame () {
+        currentState = 1;
+        BeginRoot.SetActive (false);
+        BattleRoot.SetActive (true);
+        EndRoot.SetActive (false);
+
+        this.currentNum = 0;
+    }
+
+    public void EndGame () {
+        currentState = 2;
+        BeginRoot.SetActive (false);
+        BattleRoot.SetActive (true);
+        EndRoot.SetActive (false);
+
+    }
+}
