a9a6884286b23eb8220a8970a20ffecaca600380
scienc <song_tiefei@126.com>
2020-08-21 01:22:13 +0800

    update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,11 +4,14 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
     public GameObject BeginRoot;
     public GameObject BattleRoot;
     public Text numLbale;
+    private Sequence numTween;
     public Text timeLabel;
+    private Sequence timeTween;
     public UGUISpriteAnimation scoreFx;
 
     public GameObject EndRoot;
@@ -19,74 +22,103 @@ public class UIManager : MonoBehaviour {
     public int currentTime = 0;
     public int MaxTime = 60;
 
-    public void RefreshGame () {
+    void Start()
+    {
+        timeTween = DOTween.Sequence();
+        numTween = DOTween.Sequence();
+    }
+
+    public void RefreshGame()
+    {
         currentState = 0;
-        BeginRoot.SetActive (true);
-        BattleRoot.SetActive (false);
-        EndRoot.SetActive (false);
+        BeginRoot.SetActive(true);
+        BattleRoot.SetActive(false);
+        EndRoot.SetActive(false);
 
         currentNum = 0;
         currentTime = 0;
-        CancelInvoke ("RefreshTime");
+        CancelInvoke("RefreshTime");
     }
 
-    public void BeginGame () {
+    public void BeginGame()
+    {
         currentState = 1;
-        BeginRoot.SetActive (false);
-        BattleRoot.SetActive (true);
-        EndRoot.SetActive (false);
+        BeginRoot.SetActive(false);
+        BattleRoot.SetActive(true);
+        EndRoot.SetActive(false);
 
         this.currentNum = 0;
         this.currentTime = 0;
-        numLbale.text = this.currentNum.ToString ();
-        CancelInvoke ("RefreshTime");
-        InvokeRepeating ("RefreshTime", 0, 1);
+        numLbale.text = this.currentNum.ToString();
+        CancelInvoke("RefreshTime");
+        InvokeRepeating("RefreshTime", 0, 1);
     }
 
-    public void EndGame () {
-        CancelInvoke ("RefrehsTime");
+    public void EndGame()
+    {
+        CancelInvoke("RefrehsTime");
         currentState = 2;
-        BeginRoot.SetActive (false);
-        BattleRoot.SetActive (false);
-        EndRoot.SetActive (true);
-        endNumLabel.text = currentNum.ToString ();
-        StartCoroutine (waitBeginTime ());
+        BeginRoot.SetActive(false);
+        BattleRoot.SetActive(false);
+        EndRoot.SetActive(true);
+        endNumLabel.text = currentNum.ToString();
+        StartCoroutine(waitBeginTime());
     }
 
-    IEnumerator waitBeginTime () {
-        yield return new WaitForSeconds (5);
-        RefreshGame ();
+    IEnumerator waitBeginTime()
+    {
+        yield return new WaitForSeconds(5);
+        RefreshGame();
     }
 
-    public void RefreshTime () {
-        if (currentTime >= MaxTime) {
-            EndGame ();
+    public void RefreshTime()
+    {
+        if (currentState != 1)
+        {
             return;
         }
-        if (MaxTime - currentTime <= 5) {
-            AudioManager.PlaySe ("se_countdown");
-            timeLabel.transform.DOPunchScale (Vector3.one * 1.1f, 0.2f);
+        if (currentTime >= MaxTime)
+        {
+            EndGame();
+            return;
         }
-        timeLabel.text = (MaxTime - currentTime).ToString ();
+        if (MaxTime - currentTime <= 5)
+        {
+            AudioManager.PlaySe("se_countdown");
+            timeTween.Kill();
+            timeTween.Append(timeLabel.transform.DOScale(Vector3.one * 1.2f, 0.2f));
+            timeTween.Append(timeLabel.transform.DOScale(Vector3.one, 0.4f));
+            timeLabel.transform.localScale = Vector3.one;
+            timeTween.PlayForward();
+        }
+        timeLabel.text = (MaxTime - currentTime).ToString();
         currentTime += 1;
     }
 
-    public void AddScore () {
-        if (!scoreFx.IsPlaying) {
-            scoreFx.animFinsih = () => {
-                scoreFx.gameObject.SetActive (false);
+    public void AddScore()
+    {
+        if (!scoreFx.IsPlaying)
+        {
+            scoreFx.animFinsih = () =>
+            {
+                scoreFx.gameObject.SetActive(false);
             };
-            scoreFx.gameObject.SetActive (true);
-            scoreFx.Rewind ();
+            scoreFx.gameObject.SetActive(true);
+            scoreFx.Rewind();
         }
-        StartCoroutine (waitPlaySE ());
+        StartCoroutine(waitPlaySE());
     }
 
-    IEnumerator waitPlaySE () {
-        yield return new WaitForSeconds (0.2f);
+    IEnumerator waitPlaySE()
+    {
+        yield return new WaitForSeconds(0.2f);
         currentNum += 1;
-        numLbale.text = currentNum.ToString ();
-        numLbale.transform.DOPunchScale (new Vector3 (0.6f, 0.6f, 0.6f), 0.2f);
-        AudioManager.PlaySe ("se_number");
+        numLbale.text = currentNum.ToString();
+        numTween.Kill();
+        numTween.Append(numLbale.transform.DOScale(new Vector3(0.8f, 0.8f, 0.8f), 0.2f));
+        numTween.Append(numLbale.transform.DOScale(new Vector3(0.5f, 0.5f, 0.5f), 0.4f));
+        numLbale.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        numTween.PlayForward();
+        AudioManager.PlaySe("se_number");
     }
 }
